7bce06eff02b9774f14307c758bc4c4f0c90a0a1
Calvin Yu <yucalvin5@gmail.com>
2023-11-26 10:33:53 -0800

    changed .yml and vite.config

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -131,8 +131,6 @@ function Statistics() {
                 redirectToAuthCodeFlow(clientId);
             } else {
                 try {
-                    console.log("API Key:", import.meta.env.VITE_API_KEY);
-
                     window.history.pushState({}, null, "/");
                     const accessToken = await getAccessToken(clientId, code);
                     const [short_term_tracks, medium_term_tracks, long_term_tracks] = await Promise.all([
