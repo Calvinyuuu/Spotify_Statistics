58f017e75f58df5b2716bbbbea013666f1a35343
Calvin Yu <yucalvin5@gmail.com>
2023-11-27 13:36:46 -0800

    removed pipeline for github

---
+++ server.js
@@ -0,0 +1,18 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const port = process.env.PORT || 443;
+
+app.use('/UnWrapped', express.static(path.join(__dirname, 'dist')));
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
